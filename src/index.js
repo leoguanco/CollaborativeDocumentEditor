9ff3c5809335b54fcdaedc0e21db0c40c0ee9dca
Leo Guanco <leo.guanco@gmail.com>
2019-11-30 23:19:07 -0300

    agregado endpoint para obtener los clientes conectados a socket io y expuesta la libreria de quill cursors

--- src/index.js
+++ src/index.js
@@ -17,6 +17,19 @@ require('./sockets')(io);
 // static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/quill_cursors.js', function(req, res) {
+  res.sendFile(path.join(__dirname, '..', '/node_modules/quill-cursors/dist/quill-cursors.js'))
+})
+
+app.get('/get_clients', function(req, res) {
+  let clients = io.clients((error, clients) => {
+    if (error) throw error
+    return clients
+  })
+
+  res.send(Object.keys(clients.connected))
+})
+
 // starting the server
 server.listen(app.get('port'), () => {
   console.log('Server on port', app.get('port'));
