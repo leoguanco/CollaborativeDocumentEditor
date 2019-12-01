4b879e3e017dd740482433a1039902dc83981a86
Leo Guanco <leo.guanco@gmail.com>
2019-11-30 23:21:48 -0300

    agregado quillcursorjs, handler de seleccion y cambio de texto, get clients al inicio, socket on de newcursor, textchange, selectionchange, disconnet

--- src/public/index.js
+++ src/public/index.js
@@ -1,8 +1,78 @@
 function init() {
+  Quill.register('modules/cursors', QuillCursors)
+
   // Socket IO
   let socket = io();
+  let quill = new Quill('#machudocxs', {
+    theme: 'snow',
+    modules: {
+      cursors: {
+        transformOnTextChange: true,
+      },
+    }
+  });
 
-  $('#machudocxs').summernote();
+  const cursor = quill.getModule('cursors');
+  cursor.createCursor(socket.id, 'User 1', 'blue');
+
+  const textChangeHandler = function(quill) {
+    return function(delta, oldContents, source) {
+      if (source === 'user') {
+        socket.emit('textChange', {
+          id: socket.id,
+          delta: delta
+        })
+      }
+    }
+  }
+
+  const selectionChangeHandler = function(cursors) {
+    return function(range, oldRange, source) {
+      if (source === 'user') {
+        socket.emit('cursorChange', {
+          id: socket.id,
+          range: range
+        })
+      }
+    }
+  }
+
+  const getClients = function() {
+    fetch('/get_clients')
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        data.forEach((id) => {
+          if (socket.id !== id) {
+            cursor.createCursor(id, id, 'red')
+          }
+        })
+      })
+  }
+  getClients()
+
+  quill.on('text-change', textChangeHandler(quill))
+  quill.on('selection-change', selectionChangeHandler(cursor))
+
+  socket.on('newCursor', function(data) {
+    cursor.createCursor(data.id, data.id, 'red')
+  })
+
+  socket.on('textChange', function(data) {
+    quill.updateContents(data.delta)
+  })
+
+  socket.on('cursorChange', function(data) {
+    cursor.moveCursor(data.id, data.range)
+  })
+
+  socket.on('disconnect', function(data) {
+    cursor.removeCursor(data.id)
+  })
+
+
+  /*$('#machudocxs').summernote();
 
   let body = document.getElementById('body');
 
@@ -30,7 +100,7 @@ function init() {
     $('#machudocxs').summernote('restoreRange');
     // $('#machudocxs').on('summernote.focus', () => {
     // });
-  });
+  });*/
 }
 
 document.addEventListener('DOMContentLoaded', init);
